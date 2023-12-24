eb6d9ef6fec17c4e4afb982bbfee0d1398cbb64a
Fokstal <klabu2016@mail.ru>
2023-12-24 13:49:45 +0300

    feat: add StuffStore (Sun, 24.12.23 1:49 PM)

--- ManageWorker_API/Controllers/StuffAPIController.cs
+++ ManageWorker_API/Controllers/StuffAPIController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ManageWorker_API.Data;
 using ManageWorker_API.Models;
 using ManageWorker_API.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
@@ -15,12 +16,7 @@ namespace ManageWorker_API.Controllers
         [HttpGet]
         public IEnumerable<StuffDTO> GetStuffs()
         {
-            return new List<StuffDTO>
-            {
-                new() { Id = 1, Name = "BackEnd", CountWorker = 7 },
-                new() { Id = 2, Name = "FrontEnd", CountWorker = 4 },
-                new() { Id = 3, Name = "HR", CountWorker = 2 },
-            };
+            return StuffStore.StuffList;
         }
     }
 }
---
+++ ManageWorker_API/Data/StuffStore.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ManageWorker_API.Models.Dto;
+
+namespace ManageWorker_API.Data
+{
+    public static class StuffStore
+    {
+        private static List<StuffDTO> stuffList =
+            [
+                new() { Id = 1, Name = "BackEnd", CountWorker = 7 },
+                new() { Id = 2, Name = "FrontEnd", CountWorker = 4 },
+                new() { Id = 3, Name = "HR", CountWorker = 2 },
+            ];
+
+        public static List<StuffDTO> StuffList { get => stuffList; set => stuffList = value; }
+    }
+}
