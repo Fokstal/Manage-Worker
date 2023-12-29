8e935d87a2b44b0b00f20c0f7936e010912126c4
Labut Kirill Anatolievich / .NET Developer <96412454+Fokstal@users.noreply.github.com>
2023-12-29 16:40:12 +0300

    Revert "ADD Account Controller and DB"

--- ManageWorker_API/Models/Admin.cs
+++
@@ -1,9 +0,0 @@
-namespace ManageWorker_API.Models
-{
-    public class Admin
-    {
-        public int Id { get; set; }
-        public string? Login { get; set; }
-        public string? Password { get; set; }
-    }
-}
