fa58968c756b3faa6f8f6e635021de34a0ec4a01
Alexander Bachinskiy <okyxxxf@gmail.com>
2024-01-04 21:42:07 +0300

    feat: change stuffs styles

--- frontend/src/components/stuffsPage/stuffs.tsx
+++ frontend/src/components/stuffsPage/stuffs.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { getStuffs } from "../../slices/stuffSlice";
+import { Container, Grid, Stack, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Stuffs = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const stuffs = useAppSelector(state => state.stuff.stuffs);
 
   useEffect(() =>{
@@ -11,13 +14,51 @@ const Stuffs = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
 
-  const stuffsRender = stuffs.map((stuff) => {
+  const stuffsRender = stuffs.map((stuff, i) => {
     return (
-      <div>{stuff.name}</div>
+      <Container 
+      key={stuff.id} 
+      sx={{
+        padding: '5px',
+        borderRadius: '15px',
+        backgroundColor: '#bab6b9',
+        cursor: 'pointer',
+      }}
+      onClick={() => navigate(`${stuff.id}`)}>
+        <Typography 
+          variant="h6"
+          sx={{
+            mr: 2,
+            display: { xs: 'none', md: 'flex' },
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'inherit',
+            textDecoration: 'none',
+          }}>
+          {i+1 + '. ' + stuff.name}</Typography>
+      </Container>
     )
   });
   return (
-    <>{stuffsRender}</>
+    <Grid direction='column' alignItems='center' container>
+      <Typography 
+      variant="h3"
+      sx={{
+        mr: 2,
+        display: { xs: 'none', md: 'flex' },
+        fontFamily: 'monospace',
+        fontWeight: 700,
+        letterSpacing: '.3rem',
+        color: 'inherit',
+        textDecoration: 'none',
+      }}>
+        Stuffs:
+      </Typography>
+      <Stack>
+        {stuffsRender}
+      </Stack>
+    </Grid>
   )
 };
 
