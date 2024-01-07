4c07a100751378365bf91bb3ef56defe0c1c3881
Fokstal <klabu2016@mail.ru>
2024-01-07 22:58:23 +0300

    fix: config of Docker and Docker-Compose (Sun 07.01.24 10:57 PM)

--- ManageWorker_API/obj/Release/net8.0/ManageWorker_API.AssemblyInfo.cs
+++ ManageWorker_API/obj/Release/net8.0/ManageWorker_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManageWorker_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a4c2e496dd6b7666bbd33e93eb3c7799c43fdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+663bdb73ebe1cd1e6fe087417924c4d040f6d73d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManageWorker_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManageWorker_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
