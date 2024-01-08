a3de95d7b788bc8691ca2f8296fa22c65a6241f4
Alexander Bachinskiy <okyxxxf@gmail.com>
2024-01-08 15:39:53 +0300

    fix: fix profile page

--- frontend/src/components/profile/profile.tsx
+++ frontend/src/components/profile/profile.tsx
@@ -1,41 +1,8 @@
-import React, { useState } from "react";
-import { useAppDispatch } from "../../hooks/hooks";
-import { register } from "../../slices/authSlice";
+import React from "react";
 
 const Profile = () => {
-  const [login, changeLogin] = useState<string>('');
-  const [mail, changeMail] = useState<string>('');
-  const [password, changePassword] = useState<string>('');
-
-  const dispatch = useAppDispatch();
   return (
-    <div className="profile">
-      <label htmlFor="login">Login</label>
-      <input 
-      type="text" 
-      id="login"
-      value={login} 
-      onChange={e => changeLogin(e.target.value)}/>
-      <label htmlFor="mail">Mail</label>
-      <input 
-      type="email"
-      id="mail"
-      value={mail}
-      onChange={e => changeMail(e.target.value)}/>
-      <label htmlFor="password">Password</label>
-      <input 
-      type="password" 
-      id="password"
-      value={password}
-      onChange={e => changePassword(e.target.value)}/>
-      <button onClick={() => {
-        dispatch(register({
-          login : login,
-          email : mail,
-          password : password,
-        }));
-      }}>Регистрация</button>
-    </div>
+   <>Profile</>
   )
 };
 
